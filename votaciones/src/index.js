2486b1b40c692ff1a1c0eb4ac7a15dbb8e5c1064
Mariam Apaza Santillana <mariam.apaza@tecsup.edu.pe>
2021-04-01 04:26:22 -0500

    1.Votaciones: Estado good, neutral y bad - Manejo de OnClick()

--- votaciones/src/index.js
+++ votaciones/src/index.js
@@ -1,5 +1,3 @@
-import './index.css';
-import reportWebVitals from './reportWebVitals';
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
@@ -9,22 +7,59 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return (
-    <div>
-      <h2>Give Feedback</h2>
-      <button>Good</button>
-      <button>Neutral</button>
-      <button>Bad</button>
+  //Constantes de Estadísticas
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
+
+  //Funciones handlerClick Feedback
+  const handlerClickGood = () =>{
+      setGood(good + 1)
+  }
+  const handlerClickNeutral = () =>{
+      setNeutral(neutral + 1)
+  }
+  const handlerClickBad = () =>{
+      setBad(bad + 1)
+  }
+  //Funciones  Statistics
+  const AllFunction = () =>{
+     var total = good+neutral+bad+1
+     setAll(total)
+  }
+  const AverageFunction = () =>{
+    var puntGood = good*1
+    var puntNeutral = neutral*0
+    var puntBad = bad*-1
+    //var average = "g:"+(puntGood)+" - n:"+puntNeutral+" -B:"+(puntBad-1)
+    var average = ((puntGood)+puntNeutral+(puntBad-1))/(all+1)
+    setAverage(average)
+ }
+ const PositiveFunction = () =>{
+    var total = all+1
+    var porcentaje = (100*(good+1))/total
+    setPositive(porcentaje)
+}
+return (
+  <div>
+    <h2>Give Feedback</h2>
+    <button onClick={handlerClickGood}>Good</button>
+      <button onClick={handlerClickNeutral}>Neutral</button>
+      <button onClick={handlerClickBad}>Bad</button>
       <h2>Statistics</h2>
       <p>
         <strong>Good: </strong>
+        {good}
       </p>
       <p>
         <strong>Neutral: </strong>
+        {neutral}
       </p>
       <p>
         <strong>Bad: </strong>
+        {bad}
       </p>
+
     </div>
   );
 };
